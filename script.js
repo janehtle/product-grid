d103d7d825ae834792d25c6a0688153e53417fa2
Jane Le <janehtle@gmail.com>
2025-07-02 15:50:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const toasterBtn = document.getElementById("toaster-btn");
+
+toasterBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Cuisinart-Compact-2-Slice-Toaster-CPT-122/dp/B00K0ZT1BO/ref=sr_1_2_sspa?crid=1YC6PUP9PDHDB&dib=eyJ2IjoiMSJ9.n98XjwlULEKuvpTI22i0i39p6_cM8lnXcPx3-crCkiuor2zsbHxGGFeMjvKZ7OYWvrGnZ4l7Ge6bSHazk7UprP1kjp0G8j1kk6bP27V-fW-5ca-Cm_Kv5Q7dhAAnTLCMo-JxuVPpIuajhuuno6aLOLAikOuUs5ljmIRO8taQgVoeeKnN53OfEmcK0mxQWMQb-rRlxxi1CjFHSus0cZXmkHYQc9OAZSBjo-rVihamC5arcDEgTAC2sQ3MLgefpoEnNKXPNrC-ZzC4nqejRcQ3SuaIxcjXhgMQJwmgZUQx4Qo.oNvDAB2j5E7LWQ7PG6lTRdZE8ZliKHwLvCpZu1iNyFM&dib_tag=se&keywords=toaster&qid=1751405685&s=appliances&sprefix=toaster%2Cappliances%2C63&sr=1-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1", "_blank")
+});
+
+const keurigBtn = document.getElementById("keurig-btn");
+
+keurigBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Keurig-MultiStream-Technology-Dual-Position-Customizable/dp/B0892TW82K/ref=sr_1_1?crid=2KTN4PK95660Q&dib=eyJ2IjoiMSJ9.Q-jtoMTI3CXH6yXAaukmyZK29Iqj2sOTgxqXbxmRdF4vmLU9qU1VU2eme_yi7p5ZcMhtxcjwrUlTR5695b9khRS6vlPHI-rEx-lLzf5-ClULuf0MHlmFM-GetGvlWvGhDEbsnvTG-dFf6DZIpjGNtKk_V-OR9bacNDHOgsWQTGOG3ywA3o-ZLyA4RWkpjQnsXEyS8qfT31M1W9BhN7_RffhJOxaKy3MaSWSfNjBDyZmfA-5YNsVXbDmUOT5GVxmow_eYdOgfl9jnBX6m-5Ukd7VGlsD6DmoEuoX9XYvD6Xs.RsZaCStwBC5fDCNC4Q3medlM3WOYU9MXz98xJsbArho&dib_tag=se&keywords=keurig&qid=1751405812&s=appliances&sprefix=keurig%2Cappliances%2C67&sr=1-1&th=1", "_blank")
+});
+
+const blenderBtn = document.getElementById("blender-btn");
+
+blenderBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Ninja-BN751-Professional-Smoothies-Extractions/dp/B0BMGSZMW9/ref=sr_1_8?crid=3LKZFI6XY069B&dib=eyJ2IjoiMSJ9.9hiwmSgaT4TNKBZdh7p3Annf2gWK1AQ68WYHRUBbgP1ypmaaC2u_V-dJtD_l0bfSjmvjHp07dzsUpmhhe4i3bl7rUdyiF-gEZaMNGk2IicgMmTUWzmoxFKn-c5DZTT7K2RqhCLUvQksd9TbTwkmcQ6jLEHXbqpR6qvvMv4dBXAxeMarx0HFqRYgwE4AT2xERIadMiBDFKW-4y2zm1Q2ptydLf_1pLqbmo4r135YnXAcnaEcli7jT4Q8y1T1Q26LHNuFm0qJc5z2YWfxixI4D59Cs71jDLJWBszflFyVb3aI.fVW6vFXKTU2d5_qpRMhMtAWzjR0iwx2yh93IH6t69j0&dib_tag=se&keywords=blender&qid=1751478267&s=appliances&sprefix=bl%2Cappliances%2C174&sr=1-8&th=1", "_blank")
+});
+
+const crockpotBtn = document.getElementById("crockpot-btn");
+
+crockpotBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Crock-Pot-SCV700SS-Stainless-7-Quart-Manual/dp/B003OAJGJO/ref=sr_1_1?crid=2LHAPNJNMT55A&dib=eyJ2IjoiMSJ9.PwQl0utYIPiPAgWG8aZGZx7sF1anqBfqhtzSOrEuIzcdMNmktduvbuyGMM9kimryONLo3znLd18-0DVXbSE-IDJcy8d9tjsL0rOspcgRHsdY5_2E-JPSOnbVk6kkahE2ns20oyCTektydISjVB7qkvpR4qRfJ2iWNiGX73wxHCrBhukQ_feXN1vcnBeMQFf1HOWiP7XYlfXX5wGcdRIVMV3hlEKv85ot1OCh-5abWzyTU2VOQ1BrvIOeqCyTScZa1EKKzk5dXxm4we5ijBYl-KTBDR4cmqu8Z184OfnlZtI.QzBCKr0Q0-errkK8OSXiE1r8z0hm-eCqyTLoXuL55Sg&dib_tag=se&keywords=crockpot&qid=1751405968&s=appliances&sprefix=crockpot%2Cappliances%2C67&sr=1-1", "_blank")
+});
+
+const microwaveBtn = document.getElementById("microwave-btn");
+
+microwaveBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/BLACK-DECKER-EM720CB7-Microwave-Push-Button/dp/B07HGH1KG6/ref=sr_1_1?crid=XTFYY1PPH3A9&dib=eyJ2IjoiMSJ9.jj23Lj6v4D36pn8Ut0YtNWgkAf0-I3PrAs_Mb8_mzL85exGQ3yS4nUYRzvKU7KPnxmEh6FELhBZ3D6NvX7W6QmDD--BSlyBZTyC0DsIglinNMqtnto-p5EJ1or12JkQnGhHfzUdR-QXdEvi-dP-4AThexPeCjADgglr55gyTGe_bUHP4FhQABNlHAdRIo0Fc9KJ3p-KxEJzakes2N2KzME7exGJUip-_CD2zIfOXVn7yZBLXSKtOPGd81XZ1Ct-5d5e641w4yaZxbvjivKbcuIJ8yGI_MdfXrbx0Yljh8PA.QtqIvOzdmFDIW0ZbYBIjBjBfyBpcd7Wym4tDqw_5M6U&dib_tag=se&keywords=microwave&qid=1751405695&s=appliances&sprefix=mic%2Cappliances%2C66&sr=1-1&th=1", "_blank")
+});
+
+const juicerBtn = document.getElementById("juicer-btn");
+
+juicerBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Ninja-JC151-Countertop-Dishwasher-Generation/dp/B0BR8HZL9V/ref=sr_1_6?crid=DIFCSZ3LAJQM&dib=eyJ2IjoiMSJ9.AIof8NJdlrSmDJuQ4n9auQgtt_u4bRHiurGfL6lstFuwDPosj6tWvCmveWi6wmrDQTFkbPqeX0uciqebtRRePGpg4wNMv5md0CuTvkZjL5BNytvfVANPjbihi8hhrrduzmV63obShc9Mu1A8sOk0x1uLgj3FuiuPBR_BKk90l3r0_sjf4ZbNnCOfw9QC697FjSkI9JC-q9LFX9nHCWxQBA9Hinvz6ZarLIl1lag0YrsWnnnEzOzkuAnWXS2yBw0o0--xcU_og-XUYuqtrQ5UlWPLc61xBsnl9rHZGxmiseI.ytQPtdtJJfBHmOaniOjFXPMgM8sHMVuMvp5p1o0L5o0&dib_tag=se&keywords=juicer&qid=1751405866&s=appliances&sprefix=juicer%2Cappliances%2C55&sr=1-6&th=1", "_blank")
+});
+
+const fryerBtn = document.getElementById("fryer-btn");
+
+fryerBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/Ninja-AF101-Fryer-Black-gray/dp/B07FDJMC9Q/ref=sr_1_2?crid=2IKB1TKPQL5T0&dib=eyJ2IjoiMSJ9.-1WWjEEVFyiLi6snw-GSYlj3MucNkNa1UXwj3Z1rXGiHIw8Munm_9xUbnZqbDKhy_pzWMscKdjJPyTZ5vhlpzXtEfO44D2TuhYB_e7vBB6hJRuMYI5fvvG9a0LhqKlj1-Bga_l8jIpacT34ksBOTyXTbv-HxGk48t0juKQXVy9oqCU08C73LqzrmclqAeKoK4vLn1zg1M2vzNXVHGHu0enIAIJ0M21gn8LDKyxuoS7q-H042ShI7hQw8iv-yZupfN8yf8CzzmT3CO2qUv7aZAKi6pz4kSS9XOAyq5v6ftpU.Xe4bb58zwPDSfKRfQ4WB1tOCNK9g8Rj8lCfE-jh_k5M&dib_tag=se&keywords=air%2Bfryer&qid=1751405787&s=appliances&sprefix=a%2Cappliances%2C76&sr=1-2&th=1", "_blank")
+});
+
+const kettleBtn = document.getElementById("kettle-btn");
+
+kettleBtn.addEventListener("click", function() {
+    window.open("https://www.amazon.com/SUSTEAS-Electric-Kettle-Thermometer-Protection/dp/B0B53LK4XM/ref=sxin_17_pa_sp_search_thematic_sspa?content-id=amzn1.sym.7ced4058-141f-4cbd-9b1b-8bfe2ae89078%3Aamzn1.sym.7ced4058-141f-4cbd-9b1b-8bfe2ae89078&crid=1TSVX8W5KP2RP&cv_ct_cx=kettle&keywords=kettle&pd_rd_i=B0B53LK4XM&pd_rd_r=3121e5fd-07c6-4cb7-b127-4f088bf5c4e9&pd_rd_w=2WIxz&pd_rd_wg=lrHUG&pf_rd_p=7ced4058-141f-4cbd-9b1b-8bfe2ae89078&pf_rd_r=TBG0QN04NQYHFNEHAKJN&qid=1751405912&s=appliances&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=kettle%2Cappliances%2C57&sr=1-4-9428117c-b940-4daa-97e9-ad363ada7940-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&th=1", "_blank")
+});
